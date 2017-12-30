828c9512bcb975cfdaaf998d95f09345bf6a3023
brumlablo <barbora.blozonova@gmail.com>
2017-12-30 23:16:19 +0100

    python challenge 5 done :P

--- challenge.py
+++ challenge.py
@@ -1,4 +1,5 @@
 import parseurl
+import re
 
 
 def cha1():
@@ -12,27 +13,32 @@ def cha2(toDec):
             out += chr(((ord(ch) + 2) - ord("a")) % 26 + ord("a"))
         else:
             out += ch
-
     print(out)
 
 
 def cha3():
     comments = parseurl.readIt("http://www.pythonchallenge.com/pc/def/ocr.html")
-    parseurl.countrare(comments[0][50:])
+    parseurl.countrare(comments[0][50:]) #todo: fix this piggy thingy
 
 def cha4():
     comments = parseurl.readIt("http://www.pythonchallenge.com/pc/def/equality.html")
-    print(comments)
+    result = parseurl.find3(comments[0])
 
+def cha5():
+    parseurl.numberz("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345")
 
 def main():
     cha1()
-    print("-------------------------------------------------")
-    cha2("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr" \
+    print("-----------------------1--------------------------")
+    cha2("g fmn3c wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr" \
          " ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
     cha2("map")
-    print("-------------------------------------------------")
+    print("-----------------------2--------------------------")
     cha3()
+    print("-----------------------3--------------------------")
+    cha4()
+    print("-----------------------4--------------------------")
+    cha5()
 
 
 if __name__ == "__main__":
--- parseurl.py
+++ parseurl.py
@@ -3,14 +3,14 @@ import re
 
 
 def readIt(url):
-
-    #get tyhe url comments
+    # get tyhe url comments
     html = urllib.request.urlopen(url).read().decode()
     comments = re.findall("<!--(.*)-->", html, re.DOTALL)
     return comments
 
+
 def countrare(todec):
-    #count the occurence of characters in string
+    # count the occurence of characters in string
     rarity = {}
     for c in todec:
         if c in rarity:
@@ -18,14 +18,52 @@ def countrare(todec):
         else:
             rarity[c] = 1
 
-    #sort the dict by values
+    # sort the dict by values
     rarity_sorted = sorted(rarity, key=rarity.get)
     result = ""
     for k in rarity_sorted:
         if rarity[k] <= 2:
             result += k
-        #print("{} : {}".format(k, rarity[k]))
+        # print("{} : {}".format(k, rarity[k]))
     print(result)
 
 
+def find3(todec):
+    big3 = re.findall('[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]', todec, re.DOTALL)
+    result = ""
+    for c in big3:
+        result += c
+    print(result)
 
+
+# find number in html
+def findnumber(url):
+    html = urllib.request.urlopen(url).read().decode()
+    found = re.search('([0-9]+)$', html)
+    if found is None:
+        # divide by 2
+        todiv = re.search('.*=([0-9]+)$', url)
+        if todiv is not None and html == "Yes. Divide by two and keep going.":
+            return str(int(int(todiv.group(1)) / 2))
+        else:
+            # result...
+            return html
+    else:
+        return found.group(1)
+
+
+def numberz(url):
+    urlgroups = re.match('(.*=)([0-9]+)$', url)
+
+    foundNext = urlgroups[2]
+    tmp = ""
+    while True:
+        # get number of new url from html
+        foundNext = findnumber(url)
+        try:
+            float(foundNext)
+        except ValueError:
+            break
+        # form new url
+        url = urlgroups[1] + foundNext
+    print(foundNext)
